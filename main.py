6c4e1d9d091c76e0454a0313f4fbb7b21043b247
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-11-16 16:55:41 +0100

    [FIX] UI fixed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import news_ui
+import nntp_client as cli
 
 
 if __name__ == '__main__':
-    news_ui.NewsApp().run()
-
+    groupes = cli.get_groups()
+    cli.get_articles(groupes[0])
     exit(0)
--- news_ui.py
+++ news_ui.py
@@ -8,14 +8,15 @@ Builder.load_file("button_image.kv")
 class SearchButton(Widget):
 
     def launch_search(self):
-        self.ids.search_button.text = "Searched !"
         self.ids.loupe_img.source = "img/loupe-pressed.png"
 
     def reset(self):
-        self.ids.search_button.text = "Search ?"
         self.ids.loupe_img.source = 'img/loupe.png'
 
 
 class NewsApp(App):
     def build(self):
-        return SearchButton()
+        parent = Widget()
+        searchbtn = SearchButton()
+        parent.add_widget(searchbtn)
+        return parent
