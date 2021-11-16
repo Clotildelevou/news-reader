1ffebb2213cde82cacdf912ddd94a606ab9b4301
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-11-16 16:56:15 +0100

    [FEAT][WIP] Get articles

--- nntp_client.py
+++ nntp_client.py
@@ -11,11 +11,20 @@ def connect():
     return server
 
 
-def get_groups(server):
-    """Return a collection tuple ['group', 'last', 'first', 'flag']
-    of the groups existing since i was born"""
+def get_groups():
+    """Returns a collection tuple ['group', 'last', 'first', 'flag']
+    of the groups existing since i (the creator) was born"""
+    server = connect()
     resp, group_tuple = server.newgroups(datetime.date(2000, 10, 5))
     group_list = []
     for group in group_tuple:
         group_list.append(group.group)
     return group_list
+
+
+def get_articles(group):
+    """Returns a list of articles from a group"""
+    server = connect()
+    resp, articles = server.newnews(group, datetime.date(2000, 10, 5))
+    print(articles)
+    return articles
