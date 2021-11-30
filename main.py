fcbb5240ea28afcbbe4df8278ba188d2eefa5f61
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-11-30 17:12:31 +0100

    fuck you git

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import news_ui
 import nntp_client as cli
 
+DICT = 1
+ART_NUM = 0
 
 if __name__ == '__main__':
-    groupes = cli.get_groups()
-    cli.get_articles(groupes[0])
+    group_index = 1
+    news_index = 0
+    groups = cli.get_groups()
+    art_list = cli.get_group_articles(groups[group_index])
+    cli.pretty_print_article(groups[group_index], art_list[news_index][DICT]['message-id'])
     exit(0)
